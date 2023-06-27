a7b3e715463fd7fd914336ab56e97cf940cf2b9a
Maria Cândida Xavier <xavier.m@academico.ifrn.edu.br>
2023-06-27 18:07:41 -0300

    tentando fazer CRUD de filmes

---
+++ aula009/WebAppProjeto/WebAppProjetoB2023/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjetoB2023.Models
+{
+    public class Filme
+    {
+        public long FilmeId { get; set; }
+        public string Titulo { get; set; }
+        public string Sinopse { get; set; }
+        public int Duracao { get; set; }
+        public string Classificacao { get; set; }
+        public string Genero { get; set; }
+    }
+}
